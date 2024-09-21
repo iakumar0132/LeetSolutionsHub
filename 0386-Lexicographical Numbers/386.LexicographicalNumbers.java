a9d51ced026d4fe4b80a1c1f5b0a26b73483555f
Anshu Kumar <anshukumar@Anshus-MacBook.local>
2024-09-21 17:06:09 +0530

    Runtime 1 ms Beats 100.00%
    Analyze Complexity
    
    Memory
    48.66
    MB
    Beats
    28.23%

---
+++ 0386-Lexicographical Numbers/386.LexicographicalNumbers.java
@@ -0,0 +1,20 @@
+class Solution {
+    public List<Integer> lexicalOrder(int n) {
+        List<Integer> result = new ArrayList<>();
+        for (int i = 1; i < 10; i++) {
+            dfs(i, n, result);
+        }
+        return result;
+    }
+
+    private void dfs(int current, int n, List<Integer> result) {
+        if (current > n)
+            return;
+        result.add(current);
+        for (int i = 0; i < 10; i++) {
+            if (current * 10 + i > n)
+                return;
+            dfs(current * 10 + i, n, result);
+        }
+    }
+}
